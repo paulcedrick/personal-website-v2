fab1fbc0c932fa1d30d2775c92cde75b80902aac
Paul Cedrick Artigo <paulcedrick.artigo@gmail.com>
2024-10-21 20:31:02 +0800

    fix the links

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -6,26 +6,38 @@ function Footer(props: FooterProps) {
     <footer className="flex flex-col gap-4">
       <p className="text-xs font-semibold">You can follow me here</p>
       <div className="flex flex-wrap gap-4 xl:gap-8">
-        <a className="group flex items-center gap-2 text-[#aaa]" href="#">
+        <a
+          className="group flex items-center gap-2 text-[#aaa]"
+          href="https://github.com/paulcedrick"
+        >
           <FaGithub size="24px" className="group-hover:text-[#fff]" />
           <span className="text-xs underline group-hover:text-[#fff]">
             Github
           </span>
         </a>
 
-        <a className="group flex items-center gap-2 text-[#aaa]" href="#">
+        <a
+          className="group flex items-center gap-2 text-[#aaa]"
+          href="https://www.linkedin.com/in/paul-cedrick-artigo-227644a1/"
+        >
           <FaLinkedin size="24px" className="group-hover:text-[#fff]" />
           <span className="text-xs underline group-hover:text-[#fff]">
             LinkedIn
           </span>
         </a>
 
-        <a className="group flex items-center gap-2 text-[#aaa]" href="#">
+        <a
+          className="group flex items-center gap-2 text-[#aaa]"
+          href="https://x.com/porukobayashi"
+        >
           <FaXTwitter size="24px" className="group-hover:text-[#fff]" />
           <span className="text-xs underline group-hover:text-[#fff]">X</span>
         </a>
 
-        <a className="group flex items-center gap-2 text-[#aaa]" href="#">
+        <a
+          className="group flex items-center gap-2 text-[#aaa]"
+          href="https://www.threads.net/@iam.kyuuu"
+        >
           <FaThreads size="24px" className="group-hover:text-[#fff]" />
           <span className="text-xs underline group-hover:text-[#fff]">
             Threads
